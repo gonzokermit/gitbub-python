d4219ef7eb03dfa60f72585ceaafbceeaf8dc5f8
gonzokermit <62787372+gonzokermit@users.noreply.github.com>
2021-03-28 15:16:54 +0700

    Add files via upload

---
+++ basics/bmi1.py
@@ -0,0 +1,54 @@
+import tkinter
+
+root = tkinter.Tk()
+root.geometry("360x460")
+root.configure(background="Grey")
+root.title("BMI Calculator")
+
+# function for command button
+def calculate_bmi():
+    height = float(entry_height.get())
+    weight = float(entry_weight.get())
+    bmi = round(weight / (height ** 2),2)
+    label_result["text"] = "Your BMI is {}".format(bmi)
+    if bmi == 18.5 or bmi < 18.5:
+        label_result_2["text"] = "You are underweight"
+    elif bmi > 18.5 and bmi < 25:
+        label_result_2["text"] = "You are normalweight"
+    elif bmi > 25 and bmi < 29:
+        label_result_2["text"] = "You are overweight"
+    else:
+        label_result_2["text"] = "You are obesity"
+
+# Create GUI
+# first label widget
+label_header = tkinter.Label(root, text="BMI CALCULATOR", bg="Grey", font=("arial",20,"bold"))
+label_header.pack(pady=15)
+# second label widget
+label_height = tkinter.Label(root, text="Enter your Height in Meter", bg="Grey", font=("arial",12,"bold"))
+label_height.pack(pady=10)
+# first entry text widget
+entry_height = tkinter.Entry(root, justify="center", font=("arial",15,"bold"))
+entry_height.pack()
+# third label widget
+label_weight = tkinter.Label(root, text = "Enter your Weight in KG", bg="Grey", font=("arial",12,"bold"))
+label_weight.pack(pady=10)
+# second entry text widget
+entry_weight = tkinter.Entry(root, justify="center", font=("arial",15,"bold"))
+entry_weight.pack()
+
+# 1 button widget
+button_calculate = tkinter.Button(root, text="Calculate", font=("arial",12,"bold"), command=calculate_bmi)
+button_calculate.pack(pady=20) 
+# 2 button widget
+button_close = tkinter.Button(root, text="Close Window", font=("arial",12,"bold"), command=root.destroy)
+button_close.pack()
+
+# third label widget
+label_result = tkinter.Label(root, text="Result", bg="Grey", font=("arial",15,"bold"))
+label_result.pack(pady=15)
+
+label_result_2 =  tkinter.Label(root, bg="Grey", font=("arial",15,"bold"))
+label_result_2.pack(pady=10)
+
+root.mainloop()
