b3266089787d76bc581823d21d4a31c4a6f8836f
gonzokermit <62787372+gonzokermit@users.noreply.github.com>
2022-06-21 09:19:28 +0200

    Update app.py

--- python_api/weather_api_2/app.py
+++ python_api/weather_api_2/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 
-api_key = "c53e332dc516ab13f41651cb872a1a4e"
+api_key = ""
 
 city_name = "berlin"
 url = "https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}&units=metric".format(city_name,api_key)
