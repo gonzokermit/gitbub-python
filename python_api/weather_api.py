3383b90c50753f796d3ae1e5eebf9bef21a14510
gonzokermit <62787372+gonzokermit@users.noreply.github.com>
2022-06-21 09:20:04 +0200

    Update weather_api.py

--- python_api/weather_api.py
+++ python_api/weather_api.py
@@ -13,7 +13,7 @@ line()
 
 date_time = datetime.now().strftime("%d-%m-%Y - %H:%M")
 
-api_key = "c53e332dc516ab13f41651cb872a1a4e"
+api_key = ""
 city = input("Please enter a Town: ")
 url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric".format(city,api_key)
 data = requests.get(url).json()
